630aa5bb45f0e6bc6fd5d9a95704688b5a02870b
Rom <ip.pletnev@gmail.com>
2025-10-11 07:43:16 +0300

    Исправлено отображение изображений в категориях

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 window.Telegram.WebApp.ready();
-window.Telegram.WebApp.MainButton.setText('Перейти к оплате').show();
+window.Telegram.WebApp.expand();
+window.Telegram.WebApp.setBackgroundColor('#f0f0f0');
+window.Telegram.WebApp.setHeaderColor('#f0f0f0');
+window.Telegram.WebApp.MainButton.setText('Перейти к покупкам').show();
+console.log('Telegram WebApp загружен');
+document.body.insertAdjacentHTML('beforeend', '<p>Тест: Каталог категорий загружен</p>');
