c430bf32064b9b5179e1be0b6e53015b54f10e29
Rom <ip.pletnev@gmail.com>
2025-10-15 19:47:24 +0300

    Изменение файлов

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const button = document.getElementById('goButton');
+const homeButton = document.getElementById('homeButton');
 
-    // Добавляем обработчик клика
-    button.addEventListener('click', () => {
-      // Переход на другую страницу
-      window.location.href = 'page2.html';
-    });
+homeButton.addEventListener('click', () => {
+  // Отправляем данные в Telegram или закрываем WebApp
+  if (window.Telegram.WebApp) {
+    Telegram.WebApp.close(); // Закрывает мини-приложение
+  } else {
+    console.log('Назад');
+  }
+});
