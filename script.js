62282c0fc7111d8b7a26f4a7643333d4ca9172dd
Rom <ip.pletnev@gmail.com>
2025-10-15 18:50:13 +0300

    Изменение файлов

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-window.Telegram.WebApp.ready();
-window.Telegram.WebApp.expand();
-window.Telegram.WebApp.setBackgroundColor('#f0f0f0');
-window.Telegram.WebApp.setHeaderColor('#f0f0f0');
-window.Telegram.WebApp.MainButton.setText('Перейти к покупкам').show();
-console.log('Telegram WebApp загружен');
-document.body.insertAdjacentHTML('beforeend', '<p>Тест: Каталог категорий загружен</p>');
+const button = document.getElementById('goButton');
+
+    // Добавляем обработчик клика
+    button.addEventListener('click', () => {
+      // Переход на другую страницу
+      window.location.href = 'page2.html';
+    });
